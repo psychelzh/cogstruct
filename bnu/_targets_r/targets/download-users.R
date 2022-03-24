f199f55f748f79e42dddcdcff68da2eeeffc9a6e
Liang Zhang <psychelzh@outlook.com>
2022-03-24 11:10:12 +0800

    fix issue of game version
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- bnu/_targets_r/targets/download-users.R
+++
@@ -1,4 +0,0 @@
-list(
-  tar_target(query_tmpl_users, fs::path("sql", "users.tmpl.sql"), format = "file"),
-  tar_target(users, tarflow.iquizoo::pickup(query_tmpl_users, config_where))
-)
--- bnu/_targets_r/targets/load-config.R
+++
@@ -1,4 +0,0 @@
-list(
-  tar_target(file_config, "config.yml", format = "file"),
-  tar_target(config_where, config::get("where", file = file_config))
-)
